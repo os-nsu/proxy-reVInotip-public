f4fd399441369cc909be77fec4a079732a700baf
reVInotip <grigoriy.novikov220@gmail.com>
2025-01-27 11:47:48 +0700

    Remove unused interface functions from config.h and logger.h

--- src/include/logger.h
+++ src/include/logger.h
@@ -26,22 +26,6 @@
 
 #pragma once
 
-enum LogLevel {
-    LOG_DEBUG = 1, ///< low level messages
-    LOG_INFO, ///< traces and meta information (could be substitution for
-              ///< stderr)
-    LOG_WARNING, ///< messages that describe possibly incorrect behavior with
-                 ///< saving invariant
-    LOG_ERROR, ///< messages about system errors
-    LOG_FATAL ///< messages about system errors that couldn't be processed
-};
-
-enum OutputStream {
-    STDOUT = 1,
-    STDERR,
-    FILESTREAM
-};
-
 /*!
     It initializes logger's data (path to directory with log files).
     \param [in] path path to directory with log files. Could be NULL if file stream wouldn't be used.
@@ -51,25 +35,5 @@ enum OutputStream {
     \return 0 if success, -1 and sets errno if error
 */
 int init_logger(char* path, long file_size_limit, int files_limit);
-
-/*!
-    Function frees logger's data structures
-*/
-void fini_logger(void);
-
-/*!
-    This function write message to .log file with specified format (see file
-    description)
-    \param [in] stream type of output stream   
-    \param [in] level level of message
-    \param [in] filename name of code file where write_log was called
-    \param [in] line_number number of line where write_log was called
-    \param [in] format pointer to format string (like in printf)
-    \param [in] ... variadic list of parameters for format string (like in
-    printf)
-    \return void
- */
-void write_log(enum OutputStream stream, enum LogLevel level, const char* filename, int line_number,
-               const char* format, ...);
 
 
